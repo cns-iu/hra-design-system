9fb46238184ca02be7e1c9bca7f7145552f33d84
Bruce W. Herr II <bherr@indiana.edu>
2024-01-12 13:33:57 -0500

    Updated webpage

---
+++ shared-image.js
@@ -0,0 +1,32 @@
+
+const CDN_URL='https://cdn.humanatlas.io/hra-design-system/';
+
+class SharedImage extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  get src() {
+    return this.getAttribute('src');
+  }
+
+  get cdnUrl() {
+    const baseUrl = this.getAttribute('base-url') || CDN_URL;
+    return new URL(this.src, baseUrl).toString();
+  }
+
+  get template() {
+    return `
+      <a href="${this.cdnUrl}" target="_blank" style="text-decoration: none">
+        <img src="${this.src}" style="padding: 2rem">
+      </a>
+    `
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = this.template;
+  }
+}
+
+window.customElements.define('shared-image', SharedImage);
